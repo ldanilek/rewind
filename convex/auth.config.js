b71ba7ee75fe9d4512f1d4fba228a0e2dea22e08
Lee Danilek <lee@convex.dev>
2023-07-12 10:15:01 -0700

    it compiles on convex 0.19

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+const auth = {
+  providers: [
+    {
+      domain: "https://dev-1rfqpgu8.us.auth0.com/",
+      applicationID: "hCuEQRLomvsSRrCXXuk00Rec7cylgYb8",
+    },
+  ],
+};
+export default auth;
---
+++ convex/schema.ts
@@ -0,0 +1,48 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+import { TimeFlow } from "../common";
+
+export default defineSchema({
+  moves: defineTable({
+    gameId: v.id("games"),
+    playerIndex: v.number(),
+    millisSinceStart: v.number(),
+    operation: v.number(), // enum Operation
+    realTime: v.number(),
+    userAction: v.boolean(),
+  }),
+  players: defineTable({
+    gameId: v.id("games"),
+    index: v.number(),
+    timeFlow: v.union(
+      v.literal(TimeFlow.Forward),
+      v.literal(TimeFlow.Backward)
+    ),
+    // Temporary, not persisted to db
+    formerX: v.optional(v.number()),
+    formerY: v.optional(v.number()),
+    startX: v.union(v.number(), v.null()),
+    startY: v.union(v.number(), v.null()),
+    endX: v.optional(v.union(v.number(), v.null())),
+    endY: v.optional(v.union(v.number(), v.null())),
+  }),
+  games: defineTable({
+    userId: v.id("users"),
+    level: v.number(),
+
+    // About the current player:
+    // Their index, where they started in real time and relative time, and their time flow.
+    currentPlayerIndex: v.number(),
+    // From Date.getTime(); Necessary because Convex doesn't support Date.
+    latestRealTime: v.number(),
+    latestRelativeTime: v.number(),
+    timeFlow: v.union(
+      v.literal(TimeFlow.Forward),
+      v.literal(TimeFlow.Backward)
+    ),
+  }),
+  users: defineTable({
+    name: v.string(),
+    tokenIdentifier: v.string(),
+  }).index("by_token", ["tokenIdentifier"]),
+});
